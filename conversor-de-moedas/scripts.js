8aeba632e963b7c2b5eb19c8a3246a2b81937529
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-09-29 08:54:02 -0300

    Add convert button again

--- conversor-de-moedas/scripts.js
+++ conversor-de-moedas/scripts.js
@@ -1,8 +1,14 @@
-var valorEmDolarTexto = prompt("Qual o valor em dólar que você quer converter?")
+function converter() {
+  var valorEmDolarTexto = prompt(
+    "Qual o valor em dólar que você quer converter? \n (Dólar Padrão = U$ 5.00)"
+  );
 
-var valorEmDolaNumero = parseFloat(valorEmDolarTexto)
+  var valorEmDolaNumero = parseFloat(valorEmDolarTexto);
 
-var valorEmReal = valorEmDolaNumero * 5.00
-var valorEmRealFixado = valorEmReal.toFixed(2)
+  var valorEmReal = valorEmDolaNumero * 5.0;
+  var valorEmRealFixado = valorEmReal.toFixed(2);
 
-alert(`U$ ${valorEmDolaNumero} dólares são R$ ${valorEmRealFixado} reais`)
+  alert(
+    `U$ ${valorEmDolaNumero} dólares são aproximadamente R$ ${valorEmRealFixado} reais`
+  );
+}
